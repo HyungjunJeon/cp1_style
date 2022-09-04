ca02f6507a9e598791fe69e8532b311a3e3c5fc0
Hyungjun <maru105kr@gmail.com>
2022-09-05 00:44:39 +0900

    dropdown 메뉴 선택값 넘겨주기 시도 및 README.md 추가

--- style/style/mainapp/views.py
+++ style/style/mainapp/views.py
@@ -10,10 +10,13 @@ def cody_page(request):
 
 def cody_result(request):
     if request.method == 'post':
+        print(request.POST.get())
         age = request.POST.get('age')
         gender = request.POST.get('gender')
         height = float(request.POST.get('height'))
         weight = float(request.POST.get('weight'))
-        return render(request, 'mainapp/cody/result.html', context={'age': age, 'gender': gender, 'height': height, 'weight': weight})
+        majorcategories = request.POST.get('majorcategories')
+        subcategories = request.POST.get('subcategories')
+        return render(request, 'mainapp/cody/result.html', context={'age': age, 'gender': gender, 'height': height, 'weight': weight, 'majorcategories': majorcategories, 'subcategories': subcategories})
     else:
         return render(request, 'mainapp/cody/result.html', context={'age': 'age'})
