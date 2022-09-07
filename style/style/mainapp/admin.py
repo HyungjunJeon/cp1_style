3f3dbb63df312eb6977d74b66a5147402e55319d
Hyungjun <maru105kr@gmail.com>
2022-09-07 16:36:13 +0900

    admin py error

--- style/style/mainapp/admin.py
+++ style/style/mainapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from style.style.mainapp.models import information_user
 # Register your models here.
+admin.site.register(information_user)
--- style/style/mainapp/models.py
+++ style/style/mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class information_user(models.Model):
+    gender = models.CharField(max_length=200)
+    age = models.IntegerField()
+    height = models.FloatField()
+    weight = models.FloatField()
+    large_category = models.CharField(max_length=200)
+    medium_category = models.CharField(max_length=200)
